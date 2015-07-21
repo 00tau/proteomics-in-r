cf10de886d191552b86445b35cda8e0d9071ae44
AChemist <sebastian.malchow@posteo.de>
2015-07-21 20:35:41 +0200

    Corrected Funktion addLoadings

--- skel/R/data-01-consolidation.r
+++ skel/R/data-01-consolidation.r
@@ -221,7 +221,7 @@ avrgLoading <- function(dwide) {
 #' @export
 addLoadings <- function (dwide, byRef=F) {
     avrg <- avrgLoading(dwide)
-    trgt <- ifelse(  is.na(attr(avrg, "reference") | !byRef)
+    trgt <- ifelse(  is.na(attr(avrg, "reference")) | !byRef
                    , 0
                    , mean(as.vector(log(avrg[,attr(avrg, "reference")]))))
     attr(dwide, "loadings") <- avrg
